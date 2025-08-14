a320471aeb99ef83bbbd8186067588eac05aa479
Felps3000 <filipe.snk@gmail.com>
2025-08-13 22:04:22 -0300

    função listar produtos adicionada

--- sistema_cadastro.py
+++ sistema_cadastro.py
@@ -2,18 +2,25 @@ from colorama import Fore, Back, Style
 
 # Função principal que controla o fluxo do programa
 def exibir_menu():
-    print(Fore.BLUE + "\n===SISTEMA DE CADASTRO===")
+    print(Fore.BLUE, Back.RED, Style.DIM + "\n===SISTEMA DE CADASTRO===")
     print("1. Cadastrar Produto")
     print("2. Listar Produtos")
     print("3. Sair")
+    print(Style.RESET_ALL)
 
 def cadastrar_produto(lista_produtos):
     nome = input("Digite o nome do produto: ")
     preco = float(input("Digite o preço do produto: "))
-    lista_produtos.append({"nome": nome, "preço": preco})
+    lista_produtos.append({"nome": nome, "preco": preco})
     print(f"Produto: '{nome}' cadastrado com sucesso!")
 
-
+def listar_produtos(lista_produtos):
+    if not lista_produtos:
+        print("Nenhum produto cadastrado.")
+    else:
+        print("\n=== LISTA DE PRODUTOS===")
+        for i, produto in enumerate(lista_produtos, start = 1):
+            print(f"{i}, {produto['nome']} - R$ {produto['preco']:.2f}")
 
 def main():
     lista_produtos = []
@@ -25,7 +32,7 @@ def main():
         if opcao == "1":
             cadastrar_produto(produtos)
         elif opcao == "2":
-            print("listar produtos")
+            listar_produtos(produtos)
         elif opcao == "3":
             print("Encerrando sistema ...")
             break
